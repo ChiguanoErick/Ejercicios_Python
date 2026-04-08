760e391d306c153fad192de9e1163e41b0808cd8
ChiguanoErick <erick.chiguano2020@gmail.com>
2026-04-08 13:23:58 -0500

    Actualización 2

---
+++ Actividad 2_variables.py
@@ -0,0 +1,8 @@
+#Declaración de variables en una sola línea
+primer_nombre, apellido, pais, edad, casado = 'Erick', 'Chiguano', 'Ecuador', '16', False
+print(primer_nombre, apellido, pais, edad, casado)
+print("Primer nombre:", primer_nombre)
+print("Apellido:", apellido)
+print("Pais:", pais)
+print("Edad:", edad)
+print("Casado:", casado)
