381342ebb891e738d8bf6d5500a514ec09b46d00
ChiguanoErick <erick.chiguano2020@gmail.com>
2026-04-08 12:57:19 -0500

    Actu 1

---
+++ Actividad 1.py
@@ -0,0 +1,27 @@
+#Erick Chiguano
+#08/04/2026
+#Operaciones
+print(int(3)+int(4))
+print(int(3)-int(4))
+print(int(3)*int(4))
+print(int(3)%int(4))
+print(int(3)/int(4))
+print(int(3)**int(4))
+print(int(3)//int(4))
+#Cadenas de texto
+print("Erick")
+print("Chiguano")
+print("Ecuador")
+print("Estoy disfrutando de 30 dias de Python")
+#Tipos de datos
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python', 'Finland']))
+print(type("Erick"))
+print(type("Chiguano"))
+print(type("Ecuador"))
+
+
+
--- Actividad_1.py
+++
@@ -1,4 +0,0 @@
-#Erick Chiguano
-#08/04/2026
-add=int(3+4)
-print(add)
