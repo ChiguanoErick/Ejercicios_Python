6095396d06dbe3967264f3d3df5ee2efdc9dd16f
ChiguanoErick <erick.chiguano2020@gmail.com>
2026-04-10 08:27:06 -0500

    Actividad Variables

---
+++ Variables.py
@@ -0,0 +1,8 @@
+# Erick Chiguano
+#10/04/2026
+#Ejericios: Nivel 1
+nombre, apellido, nombreCompleto, pais, ciudad, edad, año, estacasado, es verdadero, luz encendida = "Erick", "Chiguano", "Erick Chiguano", "Ecuador", "Quito", 16, 2026, "No", True, False
+print(type(nombre))
+print(type(apellido))
+print(type(nombreCompleto))
+print(type(pais))
