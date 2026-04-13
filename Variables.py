b309b95271bf559d5738c2bc12acb78120a6e0c3
ChiguanoErick <erick.chiguano2020@gmail.com>
2026-04-13 09:27:50 -0500

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,8 +1,41 @@
-# Erick Chiguano
-#10/04/2026
-#Ejericios: Nivel 1
-nombre, apellido, nombreCompleto, pais, ciudad, edad, año, estacasado, es verdadero, luz encendida = "Erick", "Chiguano", "Erick Chiguano", "Ecuador", "Quito", 16, 2026, "No", True, False
-print(type(nombre))
-print(type(apellido))
-print(type(nombreCompleto))
-print(type(pais))
+# Erick Chiguano 
+#10/04/2026 
+#Ejericios: Nivel 1 
+nombre, apellido, nombreCompleto, pais, ciudad, edad, año, esta_casado, es_verdadero, luz_encendida = "Erick", "Chiguano", "Erick Chiguano", "Ecuador", "Quito", 16, 2026, False, True, False 
+print(type(nombre)) 
+print(type(apellido)) 
+print(type(nombreCompleto)) 
+print(type(pais)) 
+print(type(ciudad)) 
+print(type(edad)) 
+print(type(año)) 
+print(type(esta_casado)) 
+print(type(es_verdadero)) 
+print(type(luz_encendida)) 
+#Ejercicios Nivel 2 
+print("Longitud de caracteres:", len(nombre)) 
+print("Longitud de caracteres:", len(apellido)) 
+numeroUno=5 
+numeroDos=4 
+total=numeroUno+numeroDos 
+diferencia=numeroUno-numeroDos 
+producto=numeroUno*numeroDos 
+division=numeroUno/numeroDos 
+residuo=numeroDos%numeroUno 
+potencia=numeroUno**numeroDos 
+divisionEntera=numeroUno//numeroDos 
+print(total, diferencia, producto, division, residuo, potencia, divisionEntera) 
+r=30 
+π=3.1416 
+areaCirculo= float(π*r**2) 
+print("Área del circulo:", areaCirculo) 
+circunferenciaCirculo= 2*π*r 
+print("Circunferencia del circulo:", circunferenciaCirculo) 
+r_user=float(input("Ingresa el radio:")) 
+area_user=float(π*r_user**2) 
+print("El area de tu circulo es:", area_user) 
+nombre_user = input("Nombre: ") 
+apellido_user = input("Apellido: ") 
+pais_user = input("País: ") 
+edad_user = input("Edad: ")  
+help('keywords') 
