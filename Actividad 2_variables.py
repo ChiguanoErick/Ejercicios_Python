7dbca429350b4c70b487a4cfecb211e957789cd0
ChiguanoErick <erick.chiguano2020@gmail.com>
2026-04-15 13:24:06 -0500

    Modificacion clase

--- Actividad 2_variables.py
+++ Actividad 2_variables.py
@@ -1,7 +1,7 @@
 #Declaración de variables en una sola línea
 primer_nombre, apellido, pais, edad, casado = 'Erick', 'Chiguano', 'Ecuador', '16', False
 print(primer_nombre, apellido, pais, edad, casado)
-print("Primer nombre:", primer_nombre)
+print(f"Primer nombre: {primer_nombre} Apellido: {apellido} Pais: {pais} Edad: {edad} Casado: {casado}" )
 print("Apellido:", apellido)
 print("Pais:", pais)
 print("Edad:", edad)
