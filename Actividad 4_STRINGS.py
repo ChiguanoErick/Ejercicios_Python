327ccc77e74a88cc22049d175acd37d0c10bb1fe
ChiguanoErick <erick.chiguano2020@gmail.com>
2026-04-20 09:23:51 -0500

    Actividad Completa

--- Actividad 4_STRINGS.py
+++ Actividad 4_STRINGS.py
@@ -1,24 +1,32 @@
-#Actividad de STRINGS
-#Nivel 1: Operaciones básicas
-texto="Programación Para Todos"
-print(texto)
-print(len(texto))
-#Nivel 2: Transformación de texto
-print(texto.upper())
-print(texto.lower())
-print(texto.title())
-print(texto.capitalize())
-#Nivel 3: Busqueda y verificación
-print(texto.startswith('Programación'))
-print(texto.endswith('Todos'))
-print(texto.find("Para"))
-print('Python'in texto)
-#Nivel 4: Manipulación
-print(texto.replace('Programación','Python'))
-print(texto.split())
-print(texto.join("_"))
-#Nivel 5: Indices
-print(texto[0])
-print(texto[-1])
-print(texto[5])
+#Actividad de STRINGS 
+#Nivel 1: Operaciones básicas 
+texto="Programación Para Todos" 
+print(texto) 
+print(len(texto)) 
+#Nivel 2: Transformación de texto 
+print(texto.upper()) 
+print(texto.lower()) 
+print(texto.title()) 
+print(texto.capitalize()) 
+#Nivel 3: Busqueda y verificación 
+print(texto.startswith('Programación')) 
+print(texto.endswith('Todos')) 
+print(texto.find("Para")) 
+print('Python'in texto) 
+#Nivel 4: Manipulación 
+print(texto.replace('Programación','Python')) 
+print(texto.split()) 
+print("-".join(texto)) 
+#Nivel 5: Indices 
+print(texto[0]) 
+print(texto[-1]) 
+print(texto[5]) 
+#Nivel 6 
+nombre="Erick Chiguano" 
+print(f"Hola, mi nombre es {nombre}") 
+iniciales = nombre.split() 
+acronimo = iniciales[0][0] + iniciales[1][0] 
+print("Acrónimo:", acronimo) 
+
+ 
 
