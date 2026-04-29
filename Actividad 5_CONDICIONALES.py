7adb3b4a90b510d62cc111636ad6b54dc76d22ed
ChiguanoErick <erick.chiguano2020@gmail.com>
2026-04-29 13:24:14 -0500

    Actividad

--- Actividad 5_CONDICIONALES.py
+++ Actividad 5_CONDICIONALES.py
@@ -1,29 +1,72 @@
-a = float(input('Ingrese un numero:'))
-if a==0:
-    print('El numero es 0')
+# Ejercicios: Nivel 1 
+edad=int(input('Ingrese su edad: ')) 
+if edad >=18: 
+    print("Tienes la edad suficiente para aprender a conducir") 
 else:
-    if a > 0 and a%2==0:
-        print('A es un numero par positvo')
-    else:
-        if a < 0 and a%2==0:
-            print('A es un numero par negativo')
-        else:
-            if a > 0 and a%2==1:
-                print('A es un numero primo positvo')
-            else:
-                print('A es un numero primo negativo')
+    diferencia= int(18-edad) 
+    print(f"Necesitas {diferencia} años más para poder aprender a conducir") 
 
-a = float(input('Ingrese un numero:'))
-if a == 0:
-    print('A es 0')
-elif a > 0 and a%2==0:
-    print('A es un numero par positvo')
-elif a < 0 and a%2==0:
-    print('A es un numero par negativo')
-elif a > 0 and a%2==1:
-    print('A es un numero primo positvo')
+ 
+my_age, your_age = 16, int(input('Ingresa tu edad: ')) 
+if my_age > your_age: 
+    dif = my_age - your_age 
+    if dif == 1: 
+        print(f'Soy {dif} año mayor que tú') 
+    else: 
+        print(f'Soy {dif} años mayor que tú') 
 else:
-    print('A es un numero primo negativo')
-    
-edad=int(input('Ingresa tu edad: '))
-print('Mayor de edad') if edad > 18 else print ('Menor de edad') 
+    if your_age > my_age: 
+        dif = your_age - my_age 
+        if dif == 1: 
+            print(f'Eres {dif} año mayor que yo') 
+        else: 
+            print(f'Eres {dif} años mayor que yo') 
+            if your_age==my_age:
+                print('WOW, tenemos la misma edad') 
+
+ 
+a = float(input("Ingrese el primer número: ")) 
+b = float(input("Ingrese el segundo número: ")) 
+if a > b: 
+    print(f"{a} es mayor que {b}") 
+elif a < b: 
+    print(f"{a} es menor que {b}") 
+else: 
+    print(f"{a} es igual a {b}") 
+
+nota = int(input("Ingrese su puntaje: ")) 
+if 90 <= nota <= 100: 
+    print("A") 
+elif 80 <= nota <= 89: 
+    print("B") 
+elif 70 <= nota <= 79: 
+    print("C") 
+elif 60 <= nota <= 69: 
+    print("D") 
+else: 
+    print("F") 
+
+ 
+ 
+mes = input("Ingrese el mes: ").lower() 
+if mes in ["septiembre", "octubre", "noviembre"]: 
+    print("Otoño") 
+elif mes in ["diciembre", "enero", "febrero"]: 
+    print("Invierno") 
+elif mes in ["marzo", "abril", "mayo"]: 
+    print("Primavera") 
+elif mes in ["junio", "julio", "agosto"]: 
+    print("Verano") 
+else: 
+    print("Mes no válido") 
+
+
+fruits = ['banana', 'orange', 'mango', 'lemon'] 
+fruta = input("Ingrese una fruta: ").lower() 
+if fruta in fruits: 
+    print("Esa fruta ya existe en la lista") 
+else: 
+    fruits.append(fruta) 
+    print("Fruta agregada:", fruits) 
+
+ 
--- desafio_python.py
+++ desafio_python.py
@@ -1,1 +1,28 @@
-
+nombre= input("Nombre: ")
+edad= int(input("Edad: "))
+puntaje= int(input("Puntaje: "))
+asistencia= float(input("% Asistencia: "))
+codigo= input("Codigo: ")
+print(nombre.upper())
+print('Caracteres:'len(nombre))
+promedio= (puntaje + asistencia) / 2
+print("El promedio es: ", promedio)
+if edad >= 14:
+    if puntaje >= 80:
+        if codigo == ("PYTHON2026"):
+            print("Resultado: Acceso VIP")
+        else:
+            print("Resultado: Acceso regular")
+    elif puntaje >= 60 and puntaje <=79:
+        print("Resultadoo: Acceso con observación")
+    else:
+        print("Resuñtado: Acceso denegado por bajo rendimiento")
+else:
+    if codigo == ("PYTHON2026"):
+        print("Resultado: Acceso con especial con acompañante")
+    else:
+        print("Resultado: No cumple la edad minima")
+if puntaje >= 90 and asistencia >= 90:
+    print("Mensaje adicional: Candidato destacado")
+elif puntaje >= 50 and asistencia >= 50:
+    print(" Mensaje adicional: Requiere refuerzo previo")
